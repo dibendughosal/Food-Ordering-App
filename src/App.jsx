1d714c72354d967926691a16fb910ced1ffd3a45
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2025-02-04 23:25:42 +0530

    Food Ordering

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,8 @@ const RestaurantCard = () => {
     <div className="restaurant-card">
       <img src="https://biryanimenu.com/wp-content/uploads/2023/11/Dada-Boudi-Biriyani-min-1024x576.png" alt="Restaurant" className='res-cardImg' />
       <h3>Dada Boudi Restaurant</h3>
-      <p>Mutton Biriyani  <span>⭐4.8</span></p>
+      <p>Mutton Biriyani</p>
+      <p>4.4 ⭐</p>
       <button>Order Now</button>
     </div>
   )
